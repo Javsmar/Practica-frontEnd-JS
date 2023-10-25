a3c50ccc41fc178a2c6e2b96bdf4240f259c60e9
Javier <mastos44@gmail.com>
2023-10-25 21:21:51 +0200

    login

---
+++ login/loginController.js
@@ -0,0 +1,33 @@
+import { loginUser } from "./loginModel.js";
+
+export const loginController = (loginForm) => {
+
+    loginForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        submitLogin(loginForm)
+    });
+}
+
+const submitLogin = async (loginForm) => {
+
+    const { email, password } = getLoginData(loginForm);
+    try {
+        const jwt = await loginUser(email, password)
+        alert('login Ok')
+        localStorage.setItem('token', jwt)
+    } catch (error) {
+        alert(error)
+    }
+}
+
+const getLoginData = (loginForm) => {
+    const formData = new FormData(loginForm);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    return {
+        email: email,
+        password: password
+    }
+}
